1c934cdf2ffa589e42d6827b9e8e9a5aab59eb2d
hnikoloski <45240953+hnikoloski@users.noreply.github.com>
2019-11-01 20:57:21 +0100

    first commit

---
+++ block.test.js
@@ -0,0 +1,11 @@
+const Block = require('./block');
+
+describe('Block', () => {
+    beforeEach(() => {
+        const data = 'bar';
+    });
+
+    it('sets the `data` to match the input', () => {});
+
+    it('sets the `lastHash` to match the hash of the last block', () => {});
+})
