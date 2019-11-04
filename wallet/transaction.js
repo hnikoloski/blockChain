b63358d7357b1f06892c1abcf53c2a60c1c293c9
hnikoloski <45240953+hnikoloski@users.noreply.github.com>
2019-11-04 11:28:47 +0100

    update transaction

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -6,6 +6,20 @@ class Transaction {
         this.input = null;
         this.outputs = [];
     }
+    update(senderWallet, recipient, amount) {
+        const senderOutput = this.outputs.find(output => output.address === senderWallet.publicKey);
+
+        if (amount > senderOutput.amount) {
+            console.log(`Amount: ${amount} exceeds balance.`);
+            return;
+        }
+
+        senderOutput.amount = senderOutput.amount - amount;
+        this.outputs.push({ amount, address: recipient });
+        Transaction.signTransaction(this, senderWallet);
+
+        return this;
+    }
 
     static newTransaction(senderWallet, recipient, amount) {
         if (amount > senderWallet.balance) {
