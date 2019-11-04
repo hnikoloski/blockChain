8c078ebd3169b64a954d1b502f554c96fccbbb04
hnikoloski <45240953+hnikoloski@users.noreply.github.com>
2019-11-04 11:53:15 +0100

    typos

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -3,9 +3,8 @@ class TransactionPool {
         this.transactions = [];
     }
 
-    updateOrAddTransaction(transactions) {
+    updateOrAddTransaction(transaction) {
         let transactionWithId = this.transactions.find(t => t.id === transaction.id);
-
         if (transactionWithId) {
             this.transactions[this.transactions.indexOf(transactionWithId)] = transaction;
         } else {
